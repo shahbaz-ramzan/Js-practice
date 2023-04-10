6317b60e6e3012537e4ca4ce56ef77ae4743c1b4
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-04-10 11:17:43 +0500

    find even number

--- april-10.js
+++ april-10.js
@@ -16,22 +16,40 @@
 
 // Create a function that takes an array of strings and returns a new array that contains only the strings that have more than 5 characters
 
-const stringArray=["shahbaz","ali","aban","shahbaz","wahab","shahab"];
+// const stringArray=["shahbaz","ali","aban","shahbaz","wahab","shahab"];
 
-function findString(stringArray){
-    let requiredString=[];
+// function findString(stringArray){
+//     let requiredString=[];
 
-    for(let i=0;i<stringArray.length;i++){
-        length=stringArray[i].length;
+//     for(let i=0;i<stringArray.length;i++){
+//         length=stringArray[i].length;
 
-        if(length>5){
-            requiredString.push(stringArray[i]);
+//         if(length>5){
+//             requiredString.push(stringArray[i]);
+//         }
+//         else{
+//             console.log(length)
+//         } 
+//     } return requiredString
+// }
+
+// let stringGreaterby5=findString(stringArray)
+// console.log(stringGreaterby5)
+
+
+
+// Create a function that takes an array of numbers and returns a new array that contains only the even numbers.
+
+let number =[2,4,6,8,4,3,2,1,2];
+
+function findEven(number){
+    let evenNumber=[];
+    for(let i=0;i<number.length;i++){
+        if(number[i]%2===0){
+            evenNumber.push(number[i])
         }
-        else{
-            console.log(length)
-        } 
-    } return requiredString
+    } return evenNumber;
 }
 
-let stringGreaterby5=findString(stringArray)
-console.log(stringGreaterby5)
+let even=findEven(number)
+console.log(even);
