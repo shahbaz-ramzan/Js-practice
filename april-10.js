ca47c1c2c90366bd7ab5cb3541454c1e4cfff6ea
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-04-10 11:02:35 +0500

    return string that have more than 5 characters

--- april-10.js
+++ april-10.js
@@ -1,14 +1,37 @@
 // Create a function that takes an array of numbers and returns the sum of all the numbers.
 
 
-let number =[2,4,6,8,4,3,2,1,2]
+// let number =[2,4,6,8,4,3,2,1,2]
 
-function findSum(number){
-    let sum =0;
-    for(let i=0;i<number.length;i++){
-        sum=sum+number[i];
-    }return sum;
+// function findSum(number){
+//     let sum =0;
+//     for(let i=0;i<number.length;i++){
+//         sum=sum+number[i];
+//     }return sum;
 
+// }
+// let sums=findSum(number);
+// console.log(sums);
+
+
+// Create a function that takes an array of strings and returns a new array that contains only the strings that have more than 5 characters
+
+const stringArray=["shahbaz","ali","aban","shahbaz","wahab","shahab"];
+
+function findString(stringArray){
+    let requiredString=[];
+
+    for(let i=0;i<stringArray.length;i++){
+        length=stringArray[i].length;
+
+        if(length>5){
+            requiredString.push(stringArray[i]);
+        }
+        else{
+            console.log(length)
+        } 
+    } return requiredString
 }
-let sums=findSum(number);
-console.log(sums)
+
+let stringGreaterby5=findString(stringArray)
+console.log(stringGreaterby5)
