46c9917671b65e5445c4e0c9972b99a2cdad8473
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-04-18 10:56:04 +0500

    april-18 not completed yet

---
+++ april-18.js
@@ -0,0 +1,68 @@
+// (1) Splice: Write a function that takes an array of strings and removes the first two elements.
+// Example Input: ['a', 'b', 'c']
+// Expected Output: ['c']
+
+// let array=["a","b","c"]
+
+// function applySplice(array){
+//     let  newarray=array.splice(0,2)
+//      return array
+// }
+
+// console.log(applySplice(array))
+
+
+
+// (2) Every: Write a function that takes an array of numbers and returns true if all the numbers are positive.
+// Example Input: [1, 2, 3]
+// Expected Output: true
+
+// let number=[1,2,3,4,5]
+
+// function checkPositive(number){
+//     return number.every((el)=>el>0)
+// }
+// console.log(checkPositive(number))
+
+//(3) Some: Write a function that takes an array of numbers and returns true if at least one number is negative.
+// Example Input: [1, -2, 3]
+// Expected Output: true
+
+
+// let input=[1,-2,3,4]
+
+// function findNegative(input){
+//     return input.some((el)=>el<0)
+// }
+
+// console.log(findNegative(input))
+
+// (4) Includes: Write a function that takes an array of strings and returns true if the array contains the string 'apple'.
+// Example Input: ['banana', 'apple', 'orange']
+// Expected Output: true
+
+
+// let array=["banana","apple","orange"]
+
+// function findValue(array){
+//     return array.includes("apple")
+// }
+
+// console.log(findValue(array))
+
+// (5) IndexOf: Write a function that takes an array of numbers and returns the index of the first number that is divisible by 3.
+// Example Input: [1, 2, 3, 4, 5, 6]
+// Expected Output: 2
+
+
+let array =[1, 2, 3, 4, 5, 6]
+
+function findDivideAble(array){
+    function divide(){
+
+    }
+    return array.indexOf(el)
+}
+
+console.log(findDivideAble(array))
+
