9f33d53c9a23565dff7758f9ed8622345d47cae5
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-04-20 22:58:57 +0500

    6 done

--- april-20.js
+++ april-20.js
@@ -49,14 +49,62 @@
 // }
 // console.log(adds(array))
 
-// (Pop: Write a function that takes an array of numbers and removes the last element from the array.
+// (4) Pop: Write a function that takes an array of numbers and removes the last element from the array.
 // Example Input: [1, 2, 3]
 // Expected Output: [1, 2]
 
-let array=[1,2,3]
+// let array=[1,2,3]
 
-function removes(array){
-     array.pop()
-     return array
+// function removes(array){
+//      array.pop()
+//      return array
+// }
+// console.log(removes(array))
+
+// (5) Shift: Write a function that takes an array of strings and removes the first element from the array.
+// Example Input: ['apple', 'banana', 'cherry']
+// Expected Output: ['banana', 'cherry']
+
+// let array = ["apple","banana","cherry"]
+
+// function removesFirst(array){
+//     array.shift()
+//     return array
+// }
+
+// console.log(removesFirst(array))
+
+
+// (6) Unshift: Write a function that takes an array of strings and adds the string 'orange' to the beginning of the array.
+// Example Input: ['apple', 'banana', 'cherry']
+// Expected Output: ['orange', 'apple', 'banana', 'cherry']
+
+// let array=["apple","banana","chery"]
+
+// function addsFirst(array){
+//     array.unshift("shahbaz")
+//     return array
+// }
+// console.log(addsFirst(array))
+
+
+// (70) FindIndex: Write a function that takes an array of objects and returns the index of the first object that has a 'name' property equal to 'Bob'.
+// Example Input: [{name: 'Alice'}, {name: 'Bob'}, {name: 'Charlie'}]
+// Expected Output: 1
+
+let array=[
+    {
+        name:"Alice"
+    },
+    {
+        name:"Bob"
+    },
+    {
+        name:"charlie"
+    }
+]
+
+function findIndex(array){
+    array.indexOf(array.name.Bob)
 }
-console.log(removes(array))
+console.log(findIndex(array))
