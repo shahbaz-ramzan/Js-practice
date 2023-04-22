f57bd9b0701c5443118f3b1633184e640ba49c43
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-04-22 17:18:49 +0500

    join method

---
+++ april-22.js
@@ -0,0 +1,16 @@
+// Join: Write a function that takes an array of strings and joins them together with a hyphen between each string.
+// Example Input: ['apple', 'banana', 'cherry']
+// Expected Output: 'apple-banana-cherry'
+
+// let array=["apple","banana","cherry"]
+
+// function tryJoin(array){
+//   newArray=  array.join( "-" )
+//   return newArray
+    
+// }
+// console.log(tryJoin(array))
+
+// Keys: Write a function that takes an object and returns an array of its values.
+// Example Input: { name: 'John', age: 30 }
+// Expected Output: ['John', 30]
