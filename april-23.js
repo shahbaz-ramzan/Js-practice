be1b49f74709424b53e4c1070df92f9da144add5
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-04-23 11:08:04 +0500

    update

--- april-23.js
+++ april-23.js
@@ -10,3 +10,30 @@
 //    return arr.reduce((a,b)=>a*b)
 // }
 // console.log(findOddProduct(array))
+
+// Write a function that takes two arrays of numbers and returns a new array with only the numbers that appear in both arrays.
+// Input: [1, 2, 3, 4, 5], [3, 4, 5, 6, 7]
+// Output: [3, 4, 5]
+
+// let arr=[1,2,3,4,5]
+// let arr1=[3,4,5,6,7]
+
+// function compareArray(arr,arr1){
+//     return arr.filter((num) => arr1.includes(num))
+    
+// }
+
+// console.log(compareArray(arr,arr1))
+
+// Write a function that takes an array of strings and returns a new array with only the strings that contain the letter 'e'.
+// Input: ['hello', 'world', 'nice', 'to', 'meet', 'you']
+// Output: ['hello', 'meet']
+
+
+// let array=['hello', 'world', 'nice', 'to', 'meet', 'you']
+
+// function findword(array){
+//     return array.filter((el)=>el.includes("e"))
+// }
+// console.log(findword(array))
+
