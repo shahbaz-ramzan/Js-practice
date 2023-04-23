64d22d3b8c9885e6c74b97db4185a53081d5c768
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-04-23 09:18:53 +0500

    april-23

---
+++ april-23.js
@@ -0,0 +1,12 @@
+// Write a function that takes an array of numbers and returns the product of all odd numbers in the array.
+// Input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// Output: 945
+
+// let array=[1,2,3,4,5,6,7,8,9,10]
+
+// function findOddProduct(array){
+//    let  arr=array.filter((el)=>el%2===1)
+
+//    return arr.reduce((a,b)=>a*b)
+// }
+// console.log(findOddProduct(array))
