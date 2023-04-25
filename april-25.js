d7c3b16156fb2a4c3b306caa70682c2fc4409468
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-04-25 20:02:03 +0500

    sorted array

--- april-25.js
+++ april-25.js
@@ -44,3 +44,14 @@
 
 // console.log(findEven(array))
 
+// Write a function that takes an array of strings and returns a new array with the strings sorted in alphabetical order.
+// Input: ['hello', 'world', 'nice', 'to', 'meet', 'you']
+// Output: ['hello', 'meet', 'nice', 'to', 'world', 'you']
+
+// let array=['hello', 'world', 'nice', 'to', 'meet', 'you']
+
+// function sortedArray(array){
+//     return array.sort()
+// }
+
+// console.log(sortedArray(array))
