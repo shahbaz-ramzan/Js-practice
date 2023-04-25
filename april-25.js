0f0e3e9f3faf6c198cb185e147068c6f0dd2e67d
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-04-25 19:51:30 +0500

    divisible

--- april-25.js
+++ april-25.js
@@ -1,10 +1,22 @@
 // Write a function that takes an array of numbers and returns a new array with the numbers in reverse order.
 // Input: [1, 2, 3, 4, 5]
 
-let array = [1,2,3,4,5]
+// let array = [1,2,3,4,5]
 
-function reverseArray(array){
-    return array.reverse()
+// function reverseArray(array){
+//     return array.reverse()
 
+// }
+// console.log(reverseArray(array))
+
+// Write a function that takes an array of numbers and returns a new array with the numbers that are divisible by 3.
+// Input: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Output: [3, 6, 9]
+
+let array=[1,2,3,4,5,6,7,8,9]
+
+function divisible(array){
+
+    return array.filter((el)=>el%3===0)
 }
-console.log(reverseArray(array))
+console.log(divisible(array))
