32e8e290ba93a91f5bb7ccb45da19c5ac8e0f26e
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-04-25 10:42:30 +0500

    reverse

---
+++ april-25.js
@@ -0,0 +1,10 @@
+// Write a function that takes an array of numbers and returns a new array with the numbers in reverse order.
+// Input: [1, 2, 3, 4, 5]
+
+let array = [1,2,3,4,5]
+
+function reverseArray(array){
+    return array.reverse()
+
+}
+console.log(reverseArray(array))
