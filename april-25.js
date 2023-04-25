eb125f5d9c125aeeb167619ac06a1d6be846e9f8
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-04-25 19:54:47 +0500

    reverseOrder

--- april-25.js
+++ april-25.js
@@ -13,10 +13,21 @@
 // Input: [1, 2, 3, 4, 5, 6, 7, 8, 9]
 // Output: [3, 6, 9]
 
-let array=[1,2,3,4,5,6,7,8,9]
+// let array=[1,2,3,4,5,6,7,8,9]
 
-function divisible(array){
+// function divisible(array){
 
-    return array.filter((el)=>el%3===0)
+//     return array.filter((el)=>el%3===0)
+// }
+// console.log(divisible(array))
+
+// Write a function that takes an array of strings and returns a new array with the strings in reverse order.
+// Input: ['hello', 'world', 'nice', 'to', 'meet', 'you']
+// Output: ['you', 'meet', 'to', 'nice', 'world', 'hello']
+
+let array= ['hello', 'world', 'nice', 'to', 'meet', 'you']
+
+function reverseOrder(array){
+    return array.reverse()
 }
-console.log(divisible(array))
+console.log(reverseOrder(array))
