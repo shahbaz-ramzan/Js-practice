ed99021b2e28a3c05142345885e29ec5b000dda2
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-04-26 18:39:24 +0500

    charAt() method

--- april-26.js
+++ april-26.js
@@ -9,3 +9,40 @@
 //     return str.length
 // }
 // console.log(findLength(str))
+
+
+// Write a function that takes a string as input and returns the string in uppercase.
+// Sample input: "hello world"
+// Expected output: "HELLO WORLD"
+
+// let str="Hello World"
+
+// function toUpper(str){
+//     return str.toUpperCase()
+// }
+
+// console.log(toUpper(str))
+
+// Write a function that takes a string as input and returns the string in lowercase.
+// Sample input: "HELLO WORLD"
+// Expected output: "hello world"
+
+// let str="HELLO WORLD"
+
+// function toLower(str){
+//     return str.toLowerCase()
+
+// }
+// console.log(toLower(str))
+
+// Write a function that takes a string as input and returns the first character of the string.
+// Sample input: "hello"
+// Expected output: "h"
+
+let str="hello"
+
+function firstLater(str){
+    return str.charAt(0)
+
+}
+console.log(firstLater(str))
