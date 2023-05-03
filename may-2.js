4bb5ac12bd2c90bf77ffec2034a1ecb21df43a18
shahbaz ramzan <mshahbazramzan952@gmail.com>
2023-05-03 11:22:40 +0500

    update

---
+++ may-2.js
@@ -0,0 +1,41 @@
+// Write a function that takes a string and returns the index of the first occurrence of a specified substring.
+// Example Input: "The quick brown fox jumps over the lazy dog.", "fox"
+// Expected Output: 16
+
+// let str="The quick brown fox jumps over the lazy dog."
+
+// function findIndex(str){
+//     return str.indexOf("fox")
+// }
+// console.log(findIndex(str))
+
+
+// Write a function that takes a string and returns the index of the last occurrence of a specified substring.
+// Example Input: "The quick brown fox jumps over the lazy dog.", "the"
+// Expected Output: 31
+
+
+// let str="The quick brown fox jumps over the lazy dog."
+
+// function findLastIndex(str){
+//     return  str.lastIndexOf("the")
+// }
+// console.log(findLastIndex(str))  // result 31
+
+
+// Write a function that takes a string and returns a new string with all occurrences of a specified substring replaced with another string.
+// Example Input: "The quick brown fox jumps over the lazy dog.", "the", "a"
+// Expected Output: "a quick brown fox jumps over a lazy dog."
+
+// let str="The quick brown fox jumps over the lazy dog."
+
+// function replace(str){
+
+// let lower=str.toLowerCase(str)
+
+// return lower.replaceAll("the","a")
+
+// }
+// console.log(replace(str))
+
+
